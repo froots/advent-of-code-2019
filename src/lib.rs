ec418db34b08b9a759518c90dadc5985e5515106
Jim Newbery <james@tinnedfruit.com>
2019-12-01 12:57:06 +0000

    Day 1: sum fuel required

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@ pub fn required_fuel(mass: &usize) -> usize {
     ((mass / 3) as usize) - 2
 }
 
+pub fn total_fuel(masses: &[usize]) -> usize {
+    masses.iter().map(|mass| required_fuel(&mass)).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn calculates_required_fuel() {
-        let data = [(12, 2), (14, 2), (1969, 654), (100756, 33583)];
-
-        for (mass, expected_fuel) in data.iter() {
-            assert_eq!(required_fuel(mass), *expected_fuel);
-        }
+    fn calculates_total_fuel() {
+        let masses = [12, 14, 1969, 100756];
+        assert_eq!(total_fuel(&masses), 34241);
     }
 }
