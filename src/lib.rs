8fb8288b80b641410c7114a1a5d4346b69ad210f
Jim Newbery <james@tinnedfruit.com>
2019-12-01 11:54:10 +0000

    Day 1: Required fuel fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+pub fn required_fuel(mass: i32) -> i32 {
+    ((mass / 3) as i32) - 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn calculates_required_fuel() {
+        assert_eq!(required_fuel(12), 2);
+        assert_eq!(required_fuel(14), 2);
+        assert_eq!(required_fuel(1969), 654);
+        assert_eq!(required_fuel(100756), 33583);
     }
 }
