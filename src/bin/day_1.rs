06236a7880dc9449d5dc46eb3a73dbd4823311eb
Jim Newbery <james@tinnedfruit.com>
2019-12-01 19:56:32 +0000

    Load input from file

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,6 +1,18 @@
-use advent_of_code_2019::d1::total_fuel;
+use advent_of_code_2019::d1;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    let masses = [45, 12, 923];
-    println!("Required fuel: {}", total_fuel(&masses));
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("./inputs/day1.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let masses: Vec<usize> = contents
+        .lines()
+        .map(|line| match line.parse::<usize>() {
+            Ok(n) => n,
+            Err(_e) => 0,
+        })
+        .collect();
+    println!("Day 1:1: {:?}", d1::total_fuel(&masses));
+    Ok(())
 }
