c8e4df5f67739d80c634158b6dd8542a46291ca8
Jim Newbery <james@tinnedfruit.com>
2019-12-02 21:28:42 +0000

    Day 2 part 2

--- src/d2.rs
+++ src/d2.rs
@@ -1,4 +1,4 @@
-pub fn part1(inp: Vec<usize>) -> Vec<usize> {
+pub fn part1(inp: &Vec<usize>) -> Vec<usize> {
     let mut computed = inp.clone();
     let mut pointer = 0;
 
@@ -19,17 +19,35 @@ pub fn part1(inp: Vec<usize>) -> Vec<usize> {
     computed
 }
 
+pub fn part2(inp: &Vec<usize>, target: usize) -> (usize, usize) {
+    let mut noun = 0;
+    let mut verb = 0;
+    'outer: for n in 0..100 {
+        'inner: for v in 0..100 {
+            let mut i = inp.clone();
+            i[1] = n;
+            i[2] = v;
+            if part1(&i)[0] == target {
+                noun = n;
+                verb = v;
+                break 'outer;
+            }
+        }
+    }
+    (noun, verb)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-        assert_eq!(part1(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
-        assert_eq!(part1(vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
+        assert_eq!(part1(&vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
+        assert_eq!(part1(&vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+        assert_eq!(part1(&vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
         assert_eq!(
-            part1(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            part1(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
             vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
         );
     }
