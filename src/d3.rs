aaa88095211295124686075ee79e91b50dd0a055
Jim Newbery <james@tinnedfruit.com>
2019-12-04 16:21:10 +0000

    Part 1 tests

--- src/d3.rs
+++ src/d3.rs
@@ -57,4 +57,13 @@ mod tests {
         assert!(wire.contains(&(1, 2)));
         assert!(wire.contains(&(1, 1)));
     }
+
+    #[test]
+    fn test_part1() {
+        let inp1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83";
+        let inp2 =
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+        assert_eq!(part1(&inp1), 159);
+        assert_eq!(part1(&inp2), 135);
+    }
 }
