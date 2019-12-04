245bfec43e66ba41a3e3168ffd9e87a6d5e08c08
Jim Newbery <james@tinnedfruit.com>
2019-12-04 19:52:38 +0000

    Adjacent digit test

--- src/d4.rs
+++ src/d4.rs
@@ -6,6 +6,21 @@ fn has_digit_size(n: usize, size: usize) -> bool {
     n.to_string().len() == size
 }
 
+fn has_adjacent_duplicate(n: usize) -> bool {
+    let digits = n.to_string();
+    let mut memo = '_';
+    let mut dupe = false;
+
+    for d in digits.chars() {
+        if d == memo {
+            dupe = true;
+        }
+        memo = d;
+    }
+
+    dupe
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,5 +29,12 @@ mod tests {
     fn test_has_digit_size() {
         assert!(has_digit_size(123456, 6));
         assert!(!has_digit_size(123, 4));
+    }
+
+    #[test]
+    fn test_has_adjacent_duplicate() {
+        assert!(has_adjacent_duplicate(123345));
+        assert!(!has_adjacent_duplicate(123456));
+        assert!(has_adjacent_duplicate(111111));
     }
 }
