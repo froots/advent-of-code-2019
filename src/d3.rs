3e85e3a2578c8f578b9a46bb0c6c565f3022a31f
Jim Newbery <james@tinnedfruit.com>
2019-12-04 16:30:48 +0000

    Share parse fn

--- src/d3.rs
+++ src/d3.rs
@@ -1,12 +1,7 @@
 use std::collections::HashSet;
 
 pub fn part1(input: &str) -> isize {
-    let wires: Vec<HashSet<(isize, isize)>> = input
-        .lines()
-        .map(|wire_input| parse_wire_input(&wire_input))
-        .collect();
-    let wire1 = wires.get(0).unwrap();
-    let wire2 = wires.get(1).unwrap();
+    let (wire1, wire2) = parse_wires(input);
     let mut distances: Vec<isize> = wire1
         .intersection(&wire2)
         .cloned()
@@ -14,6 +9,19 @@ pub fn part1(input: &str) -> isize {
         .collect();
     distances.sort();
     *distances.first().unwrap()
+}
+
+pub fn part2(input: &str) -> isize {
+    let (wire1, wire2) = parse_wires(input);
+    1
+}
+
+fn parse_wires(input: &str) -> (HashSet<(isize, isize)>, HashSet<(isize, isize)>) {
+    let wires: Vec<HashSet<(isize, isize)>> = input
+        .lines()
+        .map(|wire_input| parse_wire_input(&wire_input))
+        .collect();
+    (wires.get(0).unwrap().clone(), wires.get(1).unwrap().clone())
 }
 
 fn parse_wire_input(input: &str) -> HashSet<(isize, isize)> {
@@ -65,5 +73,14 @@ mod tests {
             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
         assert_eq!(part1(&inp1), 159);
         assert_eq!(part1(&inp2), 135);
+    }
+
+    #[test]
+    fn test_part2() {
+        let inp1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83";
+        let inp2 =
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+        assert_eq!(part2(&inp1), 610);
+        assert_eq!(part2(&inp2), 410);
     }
 }
