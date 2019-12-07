43004258d3436477df818f5189a843180db86e3a
Jim Newbery <james@tinnedfruit.com>
2019-12-07 20:20:24 +0000

    D2 tidier!

--- src/d2.rs
+++ src/d2.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-
 struct Intcode {
     state: Vec<i32>,
     pointer: usize,
@@ -13,31 +11,40 @@ impl Intcode {
         }
     }
 
-    // pub fn execute(&self) -> Result<&Vec<i32>, &str> {
-    // }
+    fn pointer_value(&self) -> &i32 {
+        self.state.get(self.pointer).unwrap()
+    }
+
+    fn get_param(&self, n: usize) -> usize {
+        self.state[self.pointer + n] as usize
+    }
+
+    fn add(&mut self) -> Vec<i32> {
+        let i1 = self.get_param(1);
+        let i2 = self.get_param(2);
+        let ri = self.get_param(3);
+        self.state[ri] = self.state[i1] + self.state[i2];
+        self.pointer += 4;
+        self.state.clone()
+    }
+
+    fn mult(&mut self) -> Vec<i32> {
+        let i1 = self.get_param(1);
+        let i2 = self.get_param(2);
+        let ri = self.get_param(3);
+        self.state[ri] = self.state[i1] * self.state[i2];
+        self.pointer += 4;
+        self.state.clone()
+    }
 }
 
 impl Iterator for Intcode {
     type Item = Vec<i32>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.state.get(self.pointer).unwrap() {
-            1 => {
-                let i1 = self.state[self.pointer + 1] as usize;
-                let i2 = self.state[self.pointer + 2] as usize;
-                let res_i = self.state[self.pointer + 3] as usize;
-                self.state[res_i] = self.state[i1] + self.state[i2];
-                self.pointer += 4;
-                Some(self.state.clone())
-            }
-            2 => {
-                let i1 = self.state[self.pointer + 1] as usize;
-                let i2 = self.state[self.pointer + 2] as usize;
-                let res_i = self.state[self.pointer + 3] as usize;
-                self.state[res_i] = self.state[i1] * self.state[i2];
-                self.pointer += 4;
-                Some(self.state.clone())
-            }
+        match self.pointer_value() {
+            1 => Some(self.add()),
+            2 => Some(self.mult()),
             99 => None,
             _ => None,
         }
@@ -46,23 +53,12 @@ impl Iterator for Intcode {
 
 pub fn part1(inp: &Vec<i32>) -> Vec<i32> {
     let computer = Intcode::new(inp.clone());
-    computer.state
-
-    // while computed[pointer] != 99 {
-    //     let i1 = computed[pointer + 1];
-    //     let i2 = computed[pointer + 2];
-    //     let res_i = computed[pointer + 3];
-
-    //     match computed[pointer] {
-    //         1 => computed[res_i] = computed[i1] + computed[i2],
-    //         2 => computed[res_i] = computed[i1] * computed[i2],
-    //         _ => panic!("Invalid opcode: {}", computed[pointer]),
-    //     }
-
-    //     pointer += 4;
-    // }
-
-    // computed
+    let mut s: Vec<i32> = Vec::new();
+    for state in computer {
+        println!("{:?}", state);
+        s = state;
+    }
+    s
 }
 
 // pub fn part2(inp: &Vec<usize>, target: usize) -> (usize, usize) {
@@ -88,22 +84,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_intcode() {
+    fn test_intcode1() {
         let mut computer = Intcode::new(vec![1, 0, 0, 0, 99]);
         assert_eq!(computer.next(), Some(vec![2, 0, 0, 0, 99]));
         assert_eq!(computer.next(), None);
     }
 
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(part1(&vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-    //     assert_eq!(part1(&vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
-    //     assert_eq!(part1(&vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
-    //     assert_eq!(
-    //         part1(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
-    //         vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
-    //     );
-    // }
+    #[test]
+    fn test_intcode2() {
+        let mut computer = Intcode::new(vec![2, 3, 0, 3, 99]);
+        assert_eq!(computer.next(), Some(vec![2, 3, 0, 6, 99]));
+        assert_eq!(computer.next(), None);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
+        assert_eq!(part1(&vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+        assert_eq!(part1(&vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
+        assert_eq!(
+            part1(&vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
+        );
+    }
 
     // #[test]
     // fn test_part2() {
