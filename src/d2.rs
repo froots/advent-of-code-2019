ffd0406c2265ad9f8bf5ca067371eb02d924e03c
Jim Newbery <james@tinnedfruit.com>
2019-12-07 21:03:04 +0000

    Extract computer to own module

--- src/d2.rs
+++ src/d2.rs
@@ -1,62 +1,9 @@
+use crate::intcode;
 use std::cmp;
 
-struct Intcode {
-    state: Vec<i32>,
-    pointer: usize,
-}
-
-impl Intcode {
-    fn new(state: Vec<i32>) -> Intcode {
-        Intcode {
-            state: state,
-            pointer: 0,
-        }
-    }
-
-    fn pointer_value(&self) -> &i32 {
-        self.state.get(self.pointer).unwrap()
-    }
-
-    fn get_param(&self, n: usize) -> usize {
-        self.state[self.pointer + n] as usize
-    }
-
-    fn add(&mut self) -> Vec<i32> {
-        let i1 = self.get_param(1);
-        let i2 = self.get_param(2);
-        let ri = self.get_param(3);
-        self.state[ri] = self.state[i1] + self.state[i2];
-        self.pointer += 4;
-        self.state.clone()
-    }
-
-    fn mult(&mut self) -> Vec<i32> {
-        let i1 = self.get_param(1);
-        let i2 = self.get_param(2);
-        let ri = self.get_param(3);
-        self.state[ri] = self.state[i1] * self.state[i2];
-        self.pointer += 4;
-        self.state.clone()
-    }
-}
-
-impl Iterator for Intcode {
-    type Item = Vec<i32>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.pointer_value() {
-            1 => Some(self.add()),
-            2 => Some(self.mult()),
-            99 => None,
-            _ => None,
-        }
-    }
-}
-
 pub fn part1(inp: &Vec<i32>) -> Vec<i32> {
-    let computer = Intcode::new(inp.clone());
-    let states: Vec<Vec<i32>> = computer.collect();
-    states.last().unwrap().clone()
+    let mut computer = intcode::Intcode::new(inp.clone());
+    computer.execute()
 }
 
 pub fn part2(inp: &Vec<i32>, target: i32) -> (i32, i32) {
@@ -80,20 +27,6 @@ pub fn part2(inp: &Vec<i32>, target: i32) -> (i32, i32) {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_intcode1() {
-        let mut computer = Intcode::new(vec![1, 0, 0, 0, 99]);
-        assert_eq!(computer.next(), Some(vec![2, 0, 0, 0, 99]));
-        assert_eq!(computer.next(), None);
-    }
-
-    #[test]
-    fn test_intcode2() {
-        let mut computer = Intcode::new(vec![2, 3, 0, 3, 99]);
-        assert_eq!(computer.next(), Some(vec![2, 3, 0, 6, 99]));
-        assert_eq!(computer.next(), None);
-    }
 
     #[test]
     fn test_part1() {
---
+++ src/intcode.rs
@@ -0,0 +1,76 @@
+pub struct Intcode {
+    state: Vec<i32>,
+    pointer: usize,
+}
+
+impl Intcode {
+    pub fn new(state: Vec<i32>) -> Intcode {
+        Intcode {
+            state: state,
+            pointer: 0,
+        }
+    }
+
+    pub fn execute(&mut self) -> Vec<i32> {
+        let states: Vec<Vec<i32>> = self.collect();
+        states.last().unwrap().clone()
+    }
+
+    fn pointer_value(&self) -> &i32 {
+        self.state.get(self.pointer).unwrap()
+    }
+
+    fn get_param(&self, n: usize) -> usize {
+        self.state[self.pointer + n] as usize
+    }
+
+    fn add(&mut self) -> Vec<i32> {
+        let i1 = self.get_param(1);
+        let i2 = self.get_param(2);
+        let ri = self.get_param(3);
+        self.state[ri] = self.state[i1] + self.state[i2];
+        self.pointer += 4;
+        self.state.clone()
+    }
+
+    fn mult(&mut self) -> Vec<i32> {
+        let i1 = self.get_param(1);
+        let i2 = self.get_param(2);
+        let ri = self.get_param(3);
+        self.state[ri] = self.state[i1] * self.state[i2];
+        self.pointer += 4;
+        self.state.clone()
+    }
+}
+
+impl Iterator for Intcode {
+    type Item = Vec<i32>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.pointer_value() {
+            1 => Some(self.add()),
+            2 => Some(self.mult()),
+            99 => None,
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_intcode1() {
+        let mut computer = Intcode::new(vec![1, 0, 0, 0, 99]);
+        assert_eq!(computer.next(), Some(vec![2, 0, 0, 0, 99]));
+        assert_eq!(computer.next(), None);
+    }
+
+    #[test]
+    fn test_intcode2() {
+        let mut computer = Intcode::new(vec![2, 3, 0, 3, 99]);
+        assert_eq!(computer.next(), Some(vec![2, 3, 0, 6, 99]));
+        assert_eq!(computer.next(), None);
+    }
+}
